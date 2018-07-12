d51f9a6a4efbc77d5e041ebbd36094204c4302b3
mlt <miltozz@gmail.com>
2018-07-12 20:12:46 +0300

    Added Components Up to Video 6

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-// let's go!
-alert('hey');
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/style.css";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#main"));
+// export default StorePicker;
